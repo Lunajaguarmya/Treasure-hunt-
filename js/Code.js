dcce164b283780631eb65760ef7dfcd98ca96c06
Lunajaguarmya <71189919+Lunajaguarmya@users.noreply.github.com>
2021-07-08 13:13:50 -0500

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("darkblue")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("HINT: Always changing, not constant !!", 100,70)
+
+    
+    fill("darkgreen")
+    textSize(15)
+    text("U T F O N N T C I", 100,50)
+    fill("lightgreen")
+    text("HINT: How somthing works !! ")
+
+    fill("darkpurple")
+    textSize(15)
+    text("T D A A A E S B", 100,50)
+    fill("lightpink")
+    text("HINT: a place where computer holds inmportant informaiton !!")
+
+}
